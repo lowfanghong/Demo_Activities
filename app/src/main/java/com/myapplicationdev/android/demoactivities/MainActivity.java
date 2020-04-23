866f9020196e2e7589bc240e8ef0a56e163f44a4
lowfanghong <18021531@myrp.edu.sg>
2020-04-23 15:40:39 +0800

    2nd commit

--- app/src/main/java/com/myapplicationdev/android/demoactivities/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoactivities/MainActivity.java
@@ -2,7 +2,11 @@ package com.myapplicationdev.android.demoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get the button and set the OnClickListener
+        Button btnDone = findViewById(R.id.button);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // Get the EditText that user keys in name
+                EditText etName = (EditText) findViewById(R.id.editText);
+                // Get the EditText that user keys in age
+                EditText etAge = (EditText) findViewById(R.id.editText2);
+                // Put the name and age into an array
+                String[] info = {etName.getText().toString(),
+                        etAge.getText().toString()};
+                // Create an intent to start another activity called
+                //  DemoActivities (which we would create later)
+                Intent i = new Intent(MainActivity.this,
+                        DemoActivities2.class);
+                // Pass the String array holding the name & age to new activity
+                i.putExtra("info", info);
+                // Start the new activity
+                startActivity(i);
+            }
+
+
+        });
     }
+
 }
